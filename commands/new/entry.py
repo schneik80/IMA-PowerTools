f36f0b9df8f07b1ddf63933bdbdb32232d831949
Kevin Schneider <schneik80@gmail.com>
2025-01-20 14:35:56 -0800

    tweak some variable names

--- commands/new/entry.py
+++ commands/new/entry.py
@@ -18,12 +18,18 @@ PANEL_ID = config.my_panel_id
 PANEL_NAME = config.my_panel_name
 PANEL_AFTER = config.my_panel_after
 
-# Pallet 
+# Pallet
 PALETTE_NAME = CMD_NAME
-PALETTE_ID = f'{config.COMPANY_NAME}_{config.ADDIN_NAME}_palette_id'
-PALETTE_URL = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'index.html')
-PALETTE_URL = PALETTE_URL.replace('\\', '/') # The path function builds a valid OS path. This fixes it to be a valid local URL.
-PALETTE_DOCKING = adsk.core.PaletteDockingStates.PaletteDockStateRight # Set a default docking behavior for the palette
+PALETTE_ID = f"{config.COMPANY_NAME}_{config.ADDIN_NAME}_palette_id"
+PALETTE_URL = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "resources", "index.html"
+)
+PALETTE_URL = PALETTE_URL.replace(
+    "\\", "/"
+)  # The path function builds a valid OS path. This fixes it to be a valid local URL.
+PALETTE_DOCKING = (
+    adsk.core.PaletteDockingStates.PaletteDockStateRight
+)  # Set a default docking behavior for the palette
 
 # Resource location for command icons, here we assume a sub folder in this directory named "resources".
 ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "")
@@ -32,6 +38,7 @@ ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resource
 local_handlers = []
 app = adsk.core.Application.get()
 ui = app.userInterface
+
 
 # Executed when add-in is run.
 def start():
@@ -48,7 +55,8 @@ def start():
     qat = ui.toolbars.itemById("QAT")
 
     # Create the command control, i.e. a button in the UI.
-    control = qat.controls.addCommand(cmd_def, "FileSubMenuCommand", False)   
+    control = qat.controls.addCommand(cmd_def, "FileSubMenuCommand", False)
+
 
 # Executed when add-in is stopped.
 def stop():
@@ -77,12 +85,16 @@ def stop():
 # You also need to connect to any command related events here.
 def command_created(args: adsk.core.CommandCreatedEventArgs):
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command created event.')
+    futil.log(f"{CMD_NAME}: Command created event.")
     futil.log(f"Hub Configured: {config.COMPANY_HUB}")
 
     # Create the event handlers you will need for this instance of the command
-    futil.add_handler(args.command.execute, command_execute, local_handlers=local_handlers)
-    futil.add_handler(args.command.destroy, command_destroy, local_handlers=local_handlers)
+    futil.add_handler(
+        args.command.execute, command_execute, local_handlers=local_handlers
+    )
+    futil.add_handler(
+        args.command.destroy, command_destroy, local_handlers=local_handlers
+    )
 
 
 # Because no command inputs are being added in the command created event, the execute
@@ -90,10 +102,10 @@ def command_created(args: adsk.core.CommandCreatedEventArgs):
 def command_execute(args: adsk.core.CommandEventArgs):
     global palette
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command execute event.')
+    futil.log(f"{CMD_NAME}: Command execute event.")
 
     palettes = ui.palettes
-    
+
     palette = palettes.itemById(PALETTE_ID)
     if palette is None:
         palette = palettes.add(
@@ -105,21 +117,22 @@ def command_execute(args: adsk.core.CommandEventArgs):
             isResizable=True,
             width=450,
             height=600,
-            useNewWebBrowser=True
+            useNewWebBrowser=True,
         )
         futil.add_handler(palette.closed, palette_closed)
         futil.add_handler(palette.navigatingURL, palette_navigating)
         futil.add_handler(palette.incomingFromHTML, palette_incoming)
-        futil.log(f'{CMD_NAME}: Created a new palette: ID = {palette.id}, Name = {palette.name}')
-
+        futil.log(
+            f"{CMD_NAME}: Created a new palette: ID = {palette.id}, Name = {palette.name}"
+        )
 
     if palette.dockingState == adsk.core.PaletteDockingStates.PaletteDockStateFloating:
         palette.dockingState = PALETTE_DOCKING
 
-        #hub check
+        # hub check
     hub = app.data.activeHub
     if hub.id != config.COMPANY_HUB:
-        futil.log(f'active hub is {hub.id}.\n{config.COMPANY_HUB} was expected')
+        futil.log(f"active hub is {hub.id}.\n{config.COMPANY_HUB} was expected")
         ui.messageBox(
             "The active hub is not configured for this command.\nPlease switch to the correct hub and try again.",
             "Incorrect Hub",
@@ -134,13 +147,13 @@ def command_execute(args: adsk.core.CommandEventArgs):
 # Use this to handle a user closing your palette.
 def palette_closed(args: adsk.core.UserInterfaceGeneralEventArgs):
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette was closed.')
+    futil.log(f"{CMD_NAME}: Palette was closed.")
 
 
 # Use this to handle a user navigating to a new page in your palette.
 def palette_navigating(args: adsk.core.NavigationEventArgs):
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette navigating event.')
+    futil.log(f"{CMD_NAME}: Palette navigating event.")
 
     # Get the URL the user is navigating to:
     url = args.navigationURL
@@ -157,13 +170,11 @@ def palette_navigating(args: adsk.core.NavigationEventArgs):
 def palette_incoming(html_args: adsk.core.HTMLEventArgs):
     global palette
 
-
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Palette incoming event.')
+    futil.log(f"{CMD_NAME}: Palette incoming event.")
 
     message_data: dict = json.loads(html_args.data)
     message_action = html_args.action
-
 
     log_msg = f"Event received from {html_args.firingEvent.sender.name}\n"
     log_msg += f"Action: {message_action}\n"
@@ -172,26 +183,27 @@ def palette_incoming(html_args: adsk.core.HTMLEventArgs):
 
     palette.isVisible = False
 
-
     docTitle = create_unique_Filename()
     docActive = app.data.activeFolder
-    doccopyurn = message_data['link']
-    docActiveUrn = app.data.findFileById(doccopyurn)
-    docNew = app.documents.open(docActiveUrn)
+    docCopyURN = message_data["link"]
+    docActiveURN = app.data.findFileById(docCopyURN)
+    docNew = app.documents.open(docActiveURN)
     docNew.saveAs(
         docTitle,
         docActive,
         "Auto created by new add-in",
-    "",
+        "",
     )
+
 
 # This event handler is called when the command terminates.
 def command_destroy(args: adsk.core.CommandEventArgs):
     # General logging for debug.
-    futil.log(f'{CMD_NAME}: Command destroy event.')
+    futil.log(f"{CMD_NAME}: Command destroy event.")
 
     global local_handlers
     local_handlers = []
+
 
 def create_unique_Filename(prefix="new"):
     # get current timestamp
